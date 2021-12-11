5ce881ca02ae2a2b83a85464fb22823d3945386b
PopChicken <jimmy.jino@hotmail.com>
2021-12-11 20:12:27 +0800

    📃 docs(util.resp): 添加文档注释
    为resp模块添加了文档注释。

--- bank_service/bot_service/service/util/resp.py
+++ bank_service/bot_service/service/util/resp.py
@@ -1,4 +1,14 @@
+"""a simple module to generate specified response
+"""
 def fail(msg: str) -> dict:
+    """when a request's response fails to make, use this
+
+    Args:
+        msg (str): message to describe details of the failure
+
+    Returns:
+        dict: the response
+    """
     resp = {
         'code': 1,
         'msg': msg,
@@ -8,6 +18,14 @@ def fail(msg: str) -> dict:
 
 
 def expire(msg: str) -> dict:
+    """DEPRECATED. when something like session expired, use this.
+
+    Args:
+        msg (str): message to describe details of the expiration
+
+    Returns:
+        dict: the response
+    """
     resp = {
         'code': 2,
         'msg': msg,
@@ -17,6 +35,15 @@ def expire(msg: str) -> dict:
 
 
 def success(resp_body: dict | list=None) -> dict:
+    """to generate the success response
+
+    Args:
+        resp_body (dict, optional): the data in response. can be a dict
+        or list. Defaults to None.
+
+    Returns:
+        dict: the response
+    """
     resp = {
         'code': 0,
         'msg': "success",
